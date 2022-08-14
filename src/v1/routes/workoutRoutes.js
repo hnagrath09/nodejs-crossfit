84db65c6de53cadb53db36aa1def7d917caefc46
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-08-14 09:37:30 +0530

    added apicache to cache workouts data

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const apicache = require('apicache')
 const workoutController = require('../../controllers/workoutController')
 const recordController = require('../../controllers/recordController')
 
 const router = express.Router()
+const cache = apicache.middleware
 
-router.get('/', workoutController.getAllWorkouts)
+router.get('/', cache('2 minutes'), workoutController.getAllWorkouts)
 
 router.get('/:workoutId', workoutController.getOneWorkout)
 
