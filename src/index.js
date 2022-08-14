14dfac88aa0443bb8f86601e5d3d7ea36c9d9c09
Himanshu Nagrath <33781906+hnagrath09@users.noreply.github.com>
2022-08-14 11:04:15 +0530

    added swagger docs for api endpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+const express = require('express')
+const bodyParser = require('body-parser')
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
+const { swaggerDocs: v1SwaggerDocs } = require('./v1/swagger')
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.use(bodyParser.json())
-app.use("/api/v1/workouts", v1WorkoutRouter)
+app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`)
+  v1SwaggerDocs(app, PORT)
 })
